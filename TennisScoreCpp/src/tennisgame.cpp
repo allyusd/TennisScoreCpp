25e1cc7d46fce6c3e267f657bccc9e801c4df275
allyusd <allyusd@yahoo.com.tw>
2018-03-06 23:20:31 +0800

    feat: FifteenAll

--- TennisScoreCpp/src/tennisgame.cpp
+++ TennisScoreCpp/src/tennisgame.cpp
@@ -10,5 +10,11 @@ std::string TennisGame::score_result(const int game_id) const
 {
 	auto game = m_repo->get_game(game_id);
 
+	if (game.first_player_score() == 1 
+		&& game.second_player_score() == 1)
+	{
+		return "Fifteen All";
+	}
+
 	return "Love All";
 }
