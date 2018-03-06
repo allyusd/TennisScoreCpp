0dd9f42c7461953878321a88c2cc1ad0564410c9
allyusd <allyusd@yahoo.com.tw>
2018-03-07 00:20:44 +0800

    feat: FirstPlayer_Advance

--- TennisScoreCpp/src/tennisgame.cpp
+++ TennisScoreCpp/src/tennisgame.cpp
@@ -15,5 +15,10 @@ std::string TennisGame::score_result(const int game_id) const
 		return game.is_deuce() ? "Deuce" : game.same_score_lookup();
 	}
 
+	if (game.first_player_score() > 3 || game.second_player_score() > 3)
+	{
+		return game.first_player_name().append(" Adv");
+	}
+
 	return game.lookup_score();
 }
