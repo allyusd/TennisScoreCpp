76387213a28b75f120ff09c956e867773fdcbe7f
allyusd <allyusd@yahoo.com.tw>
2018-03-06 23:14:27 +0800

    refactor: refactor test case

--- TennisScoreCpp/test/test.cpp
+++ TennisScoreCpp/test/test.cpp
@@ -4,27 +4,35 @@
 
 #include <gtest/gtest.h>
 
+const int any_game_id = 1;
+
 class TennisGameTest : public testing::Test
 {
 protected:
 	void SetUp() override {}
 	void TearDown() override {}
+
+	void given_game(const Game& game)
+	{
+		m_repo.set_game(game);
+	}
+
+	void score_should_be(const std::string& expected)
+	{
+		TennisGame tennis_game(&m_repo);
+		EXPECT_EQ(expected, tennis_game.score_result(any_game_id));
+	}
+
+	MockRepository m_repo;
 };
 
 TEST_F(TennisGameTest, AllLove)
 {
 	Game game;
-	game.set_id(1);
+	game.set_id(any_game_id);
 	game.set_first_player_score(0);
 	game.set_second_player_score(0);
 
-	MockRepository repo;
-	repo.set_game(game);
-
-	TennisGame tennisGame(&repo);
-
-	int game_id = 1;
-	auto result = tennisGame.score_result(game_id);
-
-	EXPECT_EQ("All Love", result);
+	given_game(game);
+	score_should_be("All Love");
 }
