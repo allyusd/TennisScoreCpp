2fb7eac4d6c3829864519d58278bdf413d8dcc88
allyusd <allyusd@yahoo.com.tw>
2018-04-29 18:56:24 +0800

    refactor: extract score_lookup function

--- TennisScoreCpp/src/tennisgame.cpp
+++ TennisScoreCpp/src/tennisgame.cpp
@@ -14,6 +14,10 @@ std::string TennisGame::score_result(const int game_id) const
 {
     auto game = m_repo->get_game(game_id);
 
-    return static_cast<std::string>(m_score_lookup_map.at(game.first_player_score()))
-        .append(" All");
+    return score_lookup(game.first_player_score()).append(" All");
+}
+
+std::string& TennisGame::score_lookup(const int score) const
+{
+    return static_cast<std::string>(m_score_lookup_map.at(score));
 }
--- TennisScoreCpp/src/tennisgame.h
+++ TennisScoreCpp/src/tennisgame.h
@@ -12,6 +12,9 @@ public:
     std::string score_result(const int game_id) const;
 
 private:
+    std::string& score_lookup(const int score) const;
+
+private:
     IRepository* m_repo;
     std::map<int, std::string> m_score_lookup_map;
 };
