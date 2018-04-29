0ada5eac140bf40ee6a09b418e9b3903c2467df6
allyusd <allyusd@yahoo.com.tw>
2018-04-29 18:45:20 +0800

    test: Thirty All case

--- TennisScoreCpp/test/test.cpp
+++ TennisScoreCpp/test/test.cpp
@@ -50,3 +50,14 @@ TEST_F(TennisGameTest, FifteenAll)
     given_game(game);
     score_should_be("Fifteen All");
 }
+
+TEST_F(TennisGameTest, ThirtyAll)
+{
+    Game game;
+    game.set_id(kAnyGameID);
+    game.set_first_player_score(2);
+    game.set_second_player_score(2);
+
+    given_game(game);
+    score_should_be("Thirty All");
+}
